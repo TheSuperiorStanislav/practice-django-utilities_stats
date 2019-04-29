f50123e656527fa100b319491f9d613207a30f6e
TheSuperiorStanislav <stasontos2@hotmail.com>
2019-04-29 17:43:09 +0700

    add edit profile view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, EditProfileView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('<int:pk>/edit', EditProfileView.as_view(), name='edit_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,25 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from .forms import UtilitiesUserCreationForm
+from .models import UtilitiesUser
 
 
 class SignUpView(CreateView):
     form_class = UtilitiesUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+class EditProfileView(UpdateView):
+    model = UtilitiesUser
+    fields = [
+        'first_name',
+        'last_name',
+        'hws_cold_water_norm',
+        'cold_water_norm',
+        'sewage_norm',
+        'electricity_norm'
+        ]
+    success_url = reverse_lazy('home')
+    template_name = 'edit_profile.html'
